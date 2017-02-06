3ab3640b4f5d71544dd4d38edc7a0df89e66a0cc
Mohammad Al-Ahdal <mkaa00x@live.co.uk>
2017-02-06 16:46:34 -0700

    Create iOS App Base

---
+++ momoToDo/momoToDo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  momoToDo
+//
+//  Created by Mohammad Al-Ahdal on 2017-02-06.
+//  Copyright © 2017 Mohammad Al-Ahdal. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
