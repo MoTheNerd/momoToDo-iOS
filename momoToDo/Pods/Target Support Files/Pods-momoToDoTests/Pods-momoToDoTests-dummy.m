9dafa71d16923dbbdfadbf17b8aa929017ca1fde
Mohammad Al-Ahdal <mkaa00x@live.co.uk>
2017-02-07 09:54:05 -0700

    Added FireBase Support
    pod ‘Firebase/Core’
    pod ‘Firebase/Database’
    pod ‘Firebase/Auth’
    
    And on firebase: authenticated google login, Facebook login, anon
    login, email login.

---
+++ momoToDo/Pods/Target Support Files/Pods-momoToDoTests/Pods-momoToDoTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_momoToDoTests : NSObject
+@end
+@implementation PodsDummy_Pods_momoToDoTests
+@end
--- momoToDo/momoToDo/AppDelegate.swift
+++ momoToDo/momoToDo/AppDelegate.swift
@@ -6,7 +6,10 @@
 //  Copyright © 2017 Mohammad Al-Ahdal. All rights reserved.
 //
 
+//https://momotodo-7104a.firebaseapp.com/__/auth/handler
+
 import UIKit
+import Firebase
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -15,7 +18,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
+        FIRApp.configure()
         return true
     }
 
